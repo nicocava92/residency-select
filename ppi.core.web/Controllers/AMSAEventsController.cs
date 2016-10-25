292783731379713252e807b89a40e81a75c95684
nicocava <nicocava92@gmail.com>
2016-10-25 12:38:28 -0200

    Base version of controller created
    Missing content in methods to test if the information was inserted
    correctly (cant use is valid for all of them since the form is devided
    up in multiple sections)

---
+++ ppi.core.web/Controllers/AMSAEventsController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PPI.Core.Web.Models;
+using PPI.Core.Web.Models.AmsaReports;
+using PPI.Core.Web.Models.AmsaReports.Event.ViewModel;
+
+namespace PPI.Core.Web.Controllers
+{
+    public class AMSAEventsController : Controller
+    {
+        public AMSAReportContext dbr { get; set; }
+
+        //Replace in Scafolding file
+        //private AMSAReportContext db = new AMSAReportContext();
+
+        public AMSAEventsController()
+        {
+            dbr = new AMSAReportContext();
+        }
+
+        // GET: /AMSAEvents/
+        public ActionResult Index()
+        {
+            var model = dbr.AMSAEvent.ToList();
+			return View(model);
+        }
+
+        // GET: /AMSAEvents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+			//replace scafolding
+            //AMSAEvent aMSAEvent = db.AMSAEvent.Find(id);
+			var model = dbr.AMSAEvent.First(m => m.id == id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        // GET: /AMSAEvents/Create
+        public ActionResult Create()
+        {
+            return View(new AMSAEventViewModel());
+        }
+
+        // POST: /AMSAEvents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AMSAEventViewModel ae)
+        {
+            if (ModelState.IsValid) //Need to change this because all information will not be present because of pagination
+            {
+                //db.AMSAEvent.Add(aMSAEvent);
+                //db.SaveChanges();
+
+                /************
+                Instaed of creating right away we should send them to a page where they can view what the entered, give them the opportunity
+                to come back or finish entering the Event
+                ***************/
+
+                
+                return RedirectToAction("CreateInsertDates",ae);
+            }
+
+            return View();
+        }
+
+        //Get page that shows date insertion for the different values related to Amsa Event
+        [HttpGet]
+        public ActionResult CreateInsertDates(AMSAEventViewModel ae) {
+            return View(ae);
+        }
+        //After dates are inserted show preview craete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateInsertDatesPost(AMSAEventViewModel ae) {
+
+            return View("PreviewCreate",ae);
+        }
+
+        //Get page that shows a preview of the information that will be stored for the AMSA Event at hand (if accepted stores)
+        [HttpGet]
+        public ActionResult PreviewCreate(AMSAEventViewModel ae)
+        {
+            return View(ae);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //Stores the event in the database
+        public ActionResult FinishCreatingEvent(AMSAEventViewModel ae)
+        {
+            //Check all of it for errors, if no errors are found then store the Event in the db
+            if (ModelState.IsValid)
+            {
+                ae.saveNewEvent();
+                return RedirectToAction("Index");
+            }
+            return View(ae);
+        }
+        
+        [HttpGet]
+        public ActionResult GoBackToInsertDates(AMSAEventViewModel ae)
+        {
+            return RedirectToAction("CreateInsertDates", ae);
+        }
+
+        [HttpGet]
+        public ActionResult GoBackToInitialCreate(AMSAEventViewModel ae)
+        {
+            return RedirectToAction("Craete", ae);
+        }
+
+        // GET: /AMSAEvents/Edit/5
+        public ActionResult Edit(int id)
+        {   
+            //AMSAEvent aMSAEvent = db.AMSAEvent.Find(id);
+			var model = dbr.AMSAEvent.First(m => m.id == id);
+            AMSAEventViewModel ae = new AMSAEventViewModel();
+            ae.AMSAEvent = model;
+            ae.loadSelectedData(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ae);
+        }
+
+        // POST: /AMSAEvents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(AMSAEventViewModel ae)
+        {
+            if (ModelState.IsValid)
+            {
+                // db.Entry(aMSAEvent).State = EntityState.Modified;
+                //db.SaveChanges();
+                ae.saveChanges();
+				return RedirectToAction("Index");
+            }
+            return View(ae);
+        }
+
+        // GET: /AMSAEvents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //AMSAEvent aMSAEvent = db.AMSAEvent.Find(id);
+			var model = dbr.AMSAEvent.First(m => m.id == id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        // POST: /AMSAEvents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+           // AMSAEvent aMSAEvent = db.AMSAEvent.Find(id);
+           // db.AMSAEvent.Remove(aMSAEvent);
+           // db.SaveChanges();
+		    var model = dbr.AMSAEvent.First(m => m.id == id);
+		    dbr.AMSAEvent.Remove(model);
+            dbr.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            //if (disposing)
+            //{
+            //    db.Dispose();
+            //}
+           // base.Dispose(disposing);
+        }
+    }
+}
---
+++ ppi.core.web/Models/AmsaReports/Event/ViewModel/AMSAEventViewModel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPI.Core.Web.Models.AmsaReports.Event.ViewModel
+{
+    public class AMSAEventViewModel
+    {
+        public AMSAEvent AMSAEvent { get; set; }
+
+        public SelectList AMSAEventType { get; set; }
+        public int idSelectedEventType { get; set; }
+        public SelectList AMSAEventStatus { get; set; }
+        public int idSelectedEventStatus { get; set; }
+        public SelectList AMSAOrganization { get; set; }
+        public int idSelectedOrganization { get; set; }
+        public SelectList AMSAProgramSite { get; set; }
+        public int idSelectedProgramSite { get; set; }
+        public SelectList AMSASurveyType { get; set; }
+        public int idSelectedSurveyType { get; set; }
+        public SelectList AMSANotBillableReason { get; set; }
+        public int idSelectedNotBillableReason { get; set; }
+
+        public AMSAEventViewModel()
+        {
+            AMSAReportContext dbr = new AMSAReportContext();
+            AMSAEventType = new SelectList(dbr.AMSAEventType.ToList(), "id", "Name");
+            AMSAEventStatus = new SelectList(dbr.AMSAEventStatus.ToList(), "id", "Name");
+            AMSAProgramSite = new SelectList(dbr.AMSAProgramSite.ToList(), "id", "Name");
+            AMSASurveyType = new SelectList(dbr.AMSASurveySiteType.ToList(), "id", "Name");
+            dbr.Dispose();
+        }
+
+        public void loadSelectedData(int id)
+        {
+            AMSAReportContext dbr = new AMSAReportContext();
+            AMSAEvent ae = dbr.AMSAEvent.Find(id);
+            idSelectedEventType = ae.AMSAEventType.id;
+            idSelectedEventStatus = ae.AMSAEventStatus.id;
+            idSelectedOrganization = ae.AMSAOrganization.id;
+            idSelectedProgramSite = ae.AMSAProgramSite.id;
+            idSelectedSurveyType = ae.AMSASurveyType.id;
+            idSelectedNotBillableReason = ae.AMSANotBillableReason.id;
+            dbr.Dispose();
+        }
+        public void saveChanges()
+        {
+            AMSAReportContext dbr = new AMSAReportContext();
+            AMSAEvent ae = dbr.AMSAEvent.Find(this.AMSAEvent.id);
+            //Store Fk to classes it points to now
+            ae.AMSAEventType = dbr.AMSAEventType.Find(this.idSelectedEventType);
+            ae.AMSAEventStatus = dbr.AMSAEventStatus.Find(this.idSelectedEventStatus);
+            ae.AMSAOrganization = dbr.AMSAOrganization.Find(this.idSelectedOrganization);
+            ae.AMSAProgramSite = dbr.AMSAProgramSite.Find(this.idSelectedProgramSite);
+            ae.AMSASurveyType = dbr.AMSASurveySiteType.Find(this.idSelectedSurveyType);
+            ae.AMSANotBillableReason = dbr.AMSANotBillableReason.Find(this.idSelectedNotBillableReason);
+            //Store changes to attributes
+            ae.Name = this.AMSAEvent.Name;
+            ae.Description = this.AMSAEvent.Description;
+            ae.StartDate = this.AMSAEvent.StartDate;
+            ae.EndDate = this.AMSAEvent.EndDate;
+            ae.ReviewDate = this.AMSAEvent.ReviewDate;
+            ae.CreateDate = this.AMSAEvent.CreateDate;
+            ae.CompositeNeedByDate = this.AMSAEvent.CompositeNeedByDate;
+            ae.JetNeedByDate = this.AMSAEvent.JetNeedByDate;
+            ae.Placement = this.AMSAEvent.Placement;
+            ae.Billable = this.AMSAEvent.Billable;
+            ae.TotalNumberOfParticipants = this.AMSAEvent.TotalNumberOfParticipants;
+            ae.defaultEmailAddress = this.AMSAEvent.defaultEmailAddress;
+            ae.OrderBy = this.AMSAEvent.OrderBy;
+            ae.JetRequired = this.AMSAEvent.JetRequired;
+            ae.CompositeRequired = this.AMSAEvent.CompositeRequired;
+
+            dbr.SaveChanges();
+            dbr.Dispose();
+        }
+        
+        public void saveNewEvent()
+        {
+            AMSAReportContext dbr = new AMSAReportContext();
+            AMSAEvent ae = new AMSAEvent();
+            ae.AMSAEventType = dbr.AMSAEventType.Find(this.idSelectedEventType);
+            ae.AMSAEventStatus = dbr.AMSAEventStatus.Find(this.idSelectedEventStatus);
+            ae.AMSAOrganization = dbr.AMSAOrganization.Find(this.idSelectedOrganization);
+            ae.AMSAProgramSite = dbr.AMSAProgramSite.Find(this.idSelectedProgramSite);
+            ae.AMSASurveyType = dbr.AMSASurveySiteType.Find(this.idSelectedSurveyType);
+            ae.AMSANotBillableReason = dbr.AMSANotBillableReason.Find(this.idSelectedNotBillableReason);
+            //Store changes to attributes
+            ae.Name = this.AMSAEvent.Name;
+            ae.Description = this.AMSAEvent.Description;
+            ae.StartDate = this.AMSAEvent.StartDate;
+            ae.EndDate = this.AMSAEvent.EndDate;
+            ae.ReviewDate = this.AMSAEvent.ReviewDate;
+            ae.CreateDate = this.AMSAEvent.CreateDate;
+            ae.CompositeNeedByDate = this.AMSAEvent.CompositeNeedByDate;
+            ae.JetNeedByDate = this.AMSAEvent.JetNeedByDate;
+            ae.Placement = this.AMSAEvent.Placement;
+            ae.Billable = this.AMSAEvent.Billable;
+            ae.TotalNumberOfParticipants = this.AMSAEvent.TotalNumberOfParticipants;
+            ae.defaultEmailAddress = this.AMSAEvent.defaultEmailAddress;
+            ae.OrderBy = this.AMSAEvent.OrderBy;
+            ae.JetRequired = this.AMSAEvent.JetRequired;
+            ae.CompositeRequired = this.AMSAEvent.CompositeRequired;
+            dbr.AMSAEvent.Add(ae);
+            dbr.Dispose();
+        }
+    }
+}
